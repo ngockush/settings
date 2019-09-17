3ebf7072fa15f3a5bda698b8d4cddb8e84123395
Cristian Tabacitu <cristitabacitu@gmail.com>
2019-09-17 10:06:15 +0300

    fixes #86 - adding the field type from json is done on all update operation methods now

--- src/app/Http/Controllers/SettingCrudController.php
+++ src/app/Http/Controllers/SettingCrudController.php
@@ -15,51 +15,54 @@ class SettingCrudController extends CrudController
         CRUD::setModel("Backpack\Settings\app\Models\Setting");
         CRUD::setEntityNameStrings(trans('backpack::settings.setting_singular'), trans('backpack::settings.setting_plural'));
         CRUD::setRoute(backpack_url('setting'));
-
-        CRUD::operation('list', function () {
-            // only show settings which are marked as active
-            CRUD::addClause('where', 'active', 1);
-
-            // columns to show in the table view
-            CRUD::setColumns([
-                [
-                    'name'  => 'name',
-                    'label' => trans('backpack::settings.name'),
-                ],
-                [
-                    'name'  => 'value',
-                    'label' => trans('backpack::settings.value'),
-                ],
-                [
-                    'name'  => 'description',
-                    'label' => trans('backpack::settings.description'),
-                ],
-            ]);
-        });
-
-        CRUD::operation('update', function () {
-            CRUD::addField([
-                'name'       => 'name',
-                'label'      => trans('backpack::settings.name'),
-                'type'       => 'text',
-                'attributes' => [
-                    'disabled' => 'disabled',
-                ],
-            ]);
-        });
     }
 
-    public function edit($id)
+    public function setupListOperation()
+    {
+        // only show settings which are marked as active
+        CRUD::addClause('where', 'active', 1);
+
+        // columns to show in the table view
+        CRUD::setColumns([
+            [
+                'name'  => 'name',
+                'label' => trans('backpack::settings.name'),
+            ],
+            [
+                'name'  => 'value',
+                'label' => trans('backpack::settings.value'),
+            ],
+            [
+                'name'  => 'description',
+                'label' => trans('backpack::settings.description'),
+            ],
+        ]);
+    }
+
+    public function setupUpdateOperation()
+    {
+        CRUD::addField([
+            'name'       => 'name',
+            'label'      => trans('backpack::settings.name'),
+            'type'       => 'text',
+            'attributes' => [
+                'disabled' => 'disabled',
+            ],
+        ]);
+
+        CRUD::addField(json_decode(CRUD::getCurrentEntry()->field, true));
+    }
+
+    public function edit()
     {
         $this->crud->hasAccessOrFail('update');
 
-        $this->data['entry'] = $this->crud->getEntry($id);
+        $this->data['entry'] = $this->crud->getCurrentEntry();
         $this->data['crud'] = $this->crud;
         $this->data['saveAction'] = $this->crud->getSaveAction();
         $this->data['title'] = trans('backpack::crud.edit').' '.$this->crud->entity_name;
-        $this->data['id'] = $id;
+        $this->data['id'] = $this->crud->getCurrentEntryId();
 
-        $this->crud->addField(json_decode($this->data['entry']->field, true));
         $this->crud->setOperationSetting('fields', $this->crud->getUpdateFields());
 
         return view($this->crud->getEditView(), $this->data);
